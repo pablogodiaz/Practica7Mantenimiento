f30dd4a900bc470523e163919fb02454a7a96b6b
AlfonsoGonzalezOrtiz <80314412+AlfonsoGonzalezOrtiz@users.noreply.github.com>
2022-05-24 12:18:41 +0200

    Primera prueba mockito

--- src/main/java/org/mps/authentication/UserRegistration.java
+++ src/main/java/org/mps/authentication/UserRegistration.java
@@ -4,14 +4,17 @@ import org.mps.authentication.CredentialValidator.ValidationStatus;
 
 public class UserRegistration {
 
-  public void register(Date birthDate, PasswordString passwordString,
-      CredentialStore credentialStore) {
+  public boolean register(Date birthDate, PasswordString passwordString,
+      CredentialStore credentialStore,CredentialValidator cv) {
 
-    var credentialValidator = new CredentialValidator(birthDate, passwordString, credentialStore);
-    ValidationStatus status = credentialValidator.validate();
+    Boolean registrado = false;
+
+    ValidationStatus status = cv.validate();
 
     if (status == ValidationStatus.VALIDATION_OK) {
       credentialStore.register(birthDate, passwordString);
+      registrado = true;
     }
+    return registrado;
   }
 }
--- src/test/java/org/mps/authentication/IntegrationTestIT.java
+++ src/test/java/org/mps/authentication/IntegrationTestIT.java
@@ -1,63 +1,26 @@
 package org.mps.authentication;
 
 
+import org.assertj.core.api.Assert;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class IntegrationTestIT {
 
     @Test
-    void ShouldDateInvalidReturnCredentialValidator_ValidationStatus_BIRTHDAY_INVALID(){
+    void prueba1(){
         UserRegistration user = new UserRegistration();
-        CredentialValidator cv = new CredentialValidator(null,null,null);
-        assertEquals(CredentialValidator.ValidationStatus.BIRTHDAY_INVALID,cv.validate());
-    }
-
-    @Test
-    void ShouldPasswordInvalidReturnCredentialValidator_ValidationStatus_PASSWORD_INVALID(){
-        UserRegistration user = new UserRegistration();
-        Date date = new Date(1,1,2022);
-        assertTrue(date.validate());
-        CredentialValidator cv = new CredentialValidator(date,null,null);
-        assertEquals(CredentialValidator.ValidationStatus.PASSWORD_INVALID,cv.validate());
-    }
-
-    @Test
-    void ShouldCredentialStoreInvalidReturnCredentialValidator_ValidationStatus_CredentialInvalid(){
-        UserRegistration user = new UserRegistration();
-        Date date = new Date(1,1,2022);
-        PasswordString ps = new PasswordString(".12345678");
-        assertTrue(date.validate());
-        assertTrue(ps.validate());
-        CredentialValidator cv = new CredentialValidator(date,ps,null);
-        assertEquals(CredentialValidator.ValidationStatus.CREDENTIAL_INVALID,cv.validate());
-    }
-
-    @Test
-    void ShouldCredentialStoreInvalidReturnCredentialValidator_ValidationStatus_ExistingCredential(){
-        UserRegistration user = new UserRegistration();
-        Date date = new Date(1,1,2022);
-        PasswordString ps = new PasswordString(".12345678");
-        CredentialStore cs = new CredentialStoreSet();
-        assertTrue(date.validate());
-        assertTrue(ps.validate());
-        assertFalse(cs.credentialExists(date,ps));
-        CredentialValidator cv = new CredentialValidator(date,ps,cs);
-        assertEquals(CredentialValidator.ValidationStatus.VALIDATION_OK,cv.validate());
-    }
-
-    @Test
-    void ShouldCredentialStoreInvalidReturnCredentialValidator_ValidationStatus_ValidationOK(){
-        UserRegistration user = new UserRegistration();
-        Date date = new Date(1,1,2022);
-        PasswordString ps = new PasswordString(".12345678");
-        CredentialStore cs = new CredentialStoreSet();
-        assertTrue(date.validate());
-        assertTrue(ps.validate());
-        cs.register(date,ps);
-        assertTrue(cs.credentialExists(date,ps));
-        CredentialValidator cv = new CredentialValidator(date,ps,cs);
-        assertEquals(CredentialValidator.ValidationStatus.EXISTING_CREDENTIAL,cv.validate());
+        Date date = Mockito.mock(Date.class);
+        PasswordString ps = Mockito.mock(PasswordString.class);
+        CredentialStore cs = Mockito.mock(CredentialStoreSet.class);
+        CredentialValidator cv = Mockito.mock(CredentialValidator.class);
+        Mockito.when(date.validate()).thenReturn(true);
+        Mockito.when(ps.validate()).thenReturn(true);
+        Mockito.when(cs.credentialExists(date,ps)).thenReturn(false);
+        Mockito.when(cv.validate()).thenReturn(CredentialValidator.ValidationStatus.VALIDATION_OK);
+        assertTrue(user.register(date,ps,cs,cv));
     }
 
 
